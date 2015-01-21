680a15424cc316e4e74edd467f274c2574fa264a
Andrea Passaglia <gurghet@gmail.com>
2015-01-21 19:36:29 +0100

    test

--- oggi.js
+++ oggi.js
@@ -12,7 +12,7 @@
 
     nonnoApp.filter('stripnbsp', function() {
 	return function(text) {
-	    return String(text).replace(/&/g, '\u00A0');
+	    return String(text).replace(/&nbsp;/g, '\u00A0');
 	};
     });
     
